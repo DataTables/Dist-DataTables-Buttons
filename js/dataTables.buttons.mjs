e9397cfaf0f4107952e3a3adce0371a422b95a68
dtbuild <dtbuild@rigil>
2024-07-19 07:35:18 +0000

    ef4a5ab11606e7dc0ecd3b6249dcb2faed302afd Release 3.1.0
    Sync to source repo @ef4a5ab11606e7dc0ecd3b6249dcb2faed302afd

--- js/dataTables.buttons.mjs
+++ js/dataTables.buttons.mjs
@@ -1,4 +1,4 @@
-/*! Buttons for DataTables 3.0.2
+/*! Buttons for DataTables 3.1.0
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -1995,7 +1995,7 @@ Buttons.defaults = {
  * @type {string}
  * @static
  */
-Buttons.version = '3.0.2';
+Buttons.version = '3.1.0';
 
 $.extend(_dtButtons, {
 	collection: {
