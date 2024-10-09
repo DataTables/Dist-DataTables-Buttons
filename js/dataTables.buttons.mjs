4f4e6585785d662a8671cc4e910d83d4e97cc260
dtbuild <dtbuild@rigil>
2024-10-09 15:15:17 +0000

    927745f3bd0ed891a69e8c006d0de368bf115a85 New: Buttons with a split can now be enabled and disabled like normal buttons
    New: Split button will disable automatically if there are no enabled buttons inside it, and then enable itself if a button is enabled (e.g. from an action such as selecting a row in the table).
    
    7db9c8c69204a0391179fa7bcac67116df10a470 Fix: Clicking on a button from a split list will now cause the collection to hide.
    
    70c236b07d2fb7c41887b178e59c10c3258fe9ab Merge branch 'master' of github.com:DataTables/Buttons
    
    Sync to source repo @70c236b07d2fb7c41887b178e59c10c3258fe9ab

--- js/dataTables.buttons.mjs
+++ js/dataTables.buttons.mjs
@@ -262,9 +262,20 @@ $.extend(Buttons.prototype, {
 	disable: function (node) {
 		var button = this._nodeToButton(node);
 
-		$(button.node)
-			.addClass(this.c.dom.button.disabled)
-			.prop('disabled', true);
+		if (button.isSplit) {
+			$(button.node.childNodes[0])
+				.addClass(this.c.dom.button.disabled)
+				.prop('disabled', true);
+		}
+		else {
+			$(button.node)
+				.addClass(this.c.dom.button.disabled)
+				.prop('disabled', true);
+		}
+
+		button.disabled = true;
+
+		this._checkSplitEnable();
 
 		return this;
 	},
@@ -315,9 +326,21 @@ $.extend(Buttons.prototype, {
 		}
 
 		var button = this._nodeToButton(node);
-		$(button.node)
-			.removeClass(this.c.dom.button.disabled)
-			.prop('disabled', false);
+
+		if (button.isSplit) {
+			$(button.node.childNodes[0])
+				.removeClass(this.c.dom.button.disabled)
+				.prop('disabled', false);
+		}
+		else {
+			$(button.node)
+				.removeClass(this.c.dom.button.disabled)
+				.prop('disabled', false);
+		}
+
+		button.disabled = false;
+
+		this._checkSplitEnable();
 
 		return this;
 	},
@@ -906,6 +929,7 @@ $.extend(Buttons.prototype, {
 				.append(button);
 
 			var dropButtonConfig = $.extend(config, {
+				autoClose: true,
 				align: dropdownConf.dropdown.align,
 				attr: {
 					'aria-haspopup': 'dialog',
@@ -982,6 +1006,57 @@ $.extend(Buttons.prototype, {
 			collection: null,
 			textNode: textNode
 		};
+	},
+
+	/**
+	 * Spin over buttons checking if splits should be enabled or not.
+	 * @param {*} buttons Array of buttons to check
+	 */
+	_checkSplitEnable: function (buttons) {
+		if (! buttons) {
+			buttons = this.s.buttons;
+		}
+
+		for (var i=0 ; i<buttons.length ; i++) {
+			var button = buttons[i];
+
+			// Check if the button is a split one and if so, determine
+			// its state
+			if (button.isSplit) {
+				var splitBtn = button.node.childNodes[1];
+
+				if (this._checkAnyEnabled(button.buttons)) {
+					// Enable the split
+					$(splitBtn)
+						.removeClass(this.c.dom.button.disabled)
+						.prop('disabled', false);
+				}
+				else {
+					$(splitBtn)
+						.addClass(this.c.dom.button.disabled)
+						.prop('disabled', false);
+				}
+			}
+			else if (button.isCollection) {
+				// Nest down into collections
+				this._checkSplitEnable(button.buttons);
+			}
+		}
+	},
+
+	/**
+	 * Check an array of buttons and see if any are enabled in it
+	 * @param {*} buttons Button array
+	 * @returns true if a button is enabled, false otherwise
+	 */
+	_checkAnyEnabled: function (buttons) {
+		for (var i=0 ; i<buttons.length ; i++) {
+			if (! buttons[i].disabled) {
+				return true;
+			}
+		}
+
+		return false;
 	},
 
 	/**
@@ -1263,6 +1338,8 @@ $.extend(Buttons.prototype, {
 			},
 			inOpts
 		);
+
+		console.log(inOpts, options);
 
 		var containerSelector =
 			options.tag + '.' + options.containerClassName.replace(/ /g, '.');
