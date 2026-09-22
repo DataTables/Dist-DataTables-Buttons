bd304a6873a8bfe46441578fcea2f8ce799e25dc
dtbuild <dtbuild@rigil>
2026-09-22 09:20:16 +0000

    45c964f2ce14dea38831a4db884603179e529f11 Release 4.1.0
    Sync to source repo @45c964f2ce14dea38831a4db884603179e529f11

--- js/dataTables.buttons.js
+++ js/dataTables.buttons.js
@@ -1,4 +1,4 @@
-/*! Buttons 4.0.3 for DataTables
+/*! Buttons 4.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
@@ -3703,7 +3703,7 @@ Buttons.defaults = {
 /**
  * Version information
  */
-Buttons.version = '4.0.3';
+Buttons.version = '4.1.0';
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * DataTables API
